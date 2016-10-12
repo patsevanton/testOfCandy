1894e6b90283f129de620eb7be2afbd97829fb47
Anton Patsev <patsev.anton@gmail.com>
2016-10-12 21:33:39 +0600

    Fix errors

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 #!usr/bin/env python
 # -*- encoding: utf-8 -*-
 import itertools
+import operator
 import random
+from functools import reduce
 
 
 def xorAllList(list):
-    out = list[0]
-    for i in range(1, len(list)):
-        out = int(out) ^ int(list[i])
-    return out
+    if len(list)> 1:
+        return reduce(operator.xor, list)
+    else:
+        return list[0]
 
-
-numberTestOfCase = random.randint(1, 10) # количество тестовых наборов
+numberTestOfCase = random.randint(1, 100) # количество тестовых наборов
 # numberTestOfCase = int(input("Введите количество тестовых наборов\n"))
-for i in range(int(numberTestOfCase)):
+for i in range(numberTestOfCase):
+    # print(i, "i")
     # numberOfCandy = int(input("Введите количество конфет\n"))
-    numberOfCandy = random.randint(1, 10)
+    numberOfCandy = random.randint(1, 100)
+    # print(numberOfCandy, "numberOfCandy")
     # weightOfCandyInList = [int(x) for x in input("Введите веса конфет\n").split()]
-    weightOfCandyInList = [random.randint(1, 10) for x in range(numberTestOfCase)]  # Введите веса конфет
-    # print(numberTestOfCase)
-    # print(weightOfCandyInList)
+    weightOfCandyInList = [random.randint(1, 1000000) for x in range(numberOfCandy)]  # Введите веса конфет
+    # print(numberTestOfCase, "numberTestOfCase")
+    # print(weightOfCandyInList, "weightOfCandyInList")
+    listOfCi = []
     for j in range(2, numberOfCandy):
         listOfList = list(itertools.combinations(weightOfCandyInList, j))
-        CiList = []
+        # print(listOfList, "listOfList")
         for k in listOfList:
             otherList = list(set(weightOfCandyInList).difference(k))
-            if xorAllList(k) == xorAllList(otherList):
-                CiList.append(sum([int(i) for i in k]))
-        print("Case {0} {1}".format(int(i), max(int(i) for i in CiList)))
+            # print(otherList, "otherList")
+            # print(k, "k")
+            if (len(k) > 0 and len(otherList) > 0):
+                if (xorAllList(k) == xorAllList(otherList)):
+                    listOfCi.append(sum([int(i) for i in k]))
+        # print(listOfCi, "listOfCi")
+    if listOfCi:
+        print("Case #{0}: {1}".format(int(i), max(listOfCi)))
+    else:
+        print("Case #{0}: NO".format(int(i)))
+    listOfCi = None
